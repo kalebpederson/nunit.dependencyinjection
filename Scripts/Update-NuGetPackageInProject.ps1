2e824912223ad804d276b42c5d5ef697882657a8
Kaleb Pederson <kaleb.pederson@gmail.com>
2018-12-10 20:36:43 -0700

    Update validation scripts. (#1)

---
+++ Scripts/Update-NuGetPackageInProject.ps1
@@ -0,0 +1,34 @@
+param(
+  [Parameter(Mandatory=$true)]
+  [string]$source,                # nuget package source
+  [Parameter(Mandatory=$true)]
+  [string]$id,                    # nuget package id
+  [Parameter(Mandatory=$true)]
+  [string]$installProject         # the project to install into
+)
+
+if (-not (test-path -type Leaf $installProject)) {
+  write-error 'installproject must be a valid file'
+  exit 1
+}
+
+if (-not (test-path -type Container $source)) {
+  write-error 'source must be a valid directory'
+  exit 2
+}
+
+$sourceDir = (resolve-path $source).Path
+pushd @(dir $installProject).Directory.FullName > $null 2>&1
+try
+{
+  write-host "Installing package $id from source $sourceDir into $installProject..."
+  dotnet add package $id -s $sourceDir
+  if ($LASTEXITCODE -ne 0)
+  {
+    exit 3
+  }
+}
+finally
+{
+  popd
+}
